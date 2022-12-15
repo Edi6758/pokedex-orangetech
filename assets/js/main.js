4df6d3d596ce84d7307f1bb6507da1b6df64414f
Edi6758 <ediarts7@gmail.com>
2022-12-15 01:16:15 -0300

    :sparkles: handling the api by callback
    handling the api by callback

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,17 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-fetch(url).then((response) => {
-    console.info(response)
-})
+fetch(url)
+    .then((response) => {
+        response
+            .json()
+            .then((responseBody)=>{
+                console.log(responseBody)
+            })
+    })
+    .catch((error) => {
+        console.info(error)
+    })
+    .finally(() => {
+        console.log("Requisição concluída!")
+    })
